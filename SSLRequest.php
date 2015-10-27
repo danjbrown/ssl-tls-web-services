19f7b14d8dc93e764ee77a0b58456fbe0ba47286
Dan Brown <dbrown.email@gmail.com>
2015-10-27 20:32:07 +0000

    PHP class for web service request

---
+++ SSLRequest.php
@@ -0,0 +1,59 @@
+<?php
+
+class SSLRequest extends Zend_Db_Table_Abstract {
+    
+    // Configuration variables: certificates, keys, passwords, etc.
+    private $userAgent = 'Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)';
+    private $certificate = '/certificates/client.crt.pem';
+    private $key = '/certificates/client.key.pem';
+    private $certificatePassword = 'password';
+    private $keyPassword = 'password';
+    
+    // Request URL: specified by the user
+    public $url = '';
+    
+    public function init() {
+        $this->response = new stdClass;
+        $this->response->error = '';
+        $this->response->data = '';
+    }
+    
+    public function request()
+    {
+        $ch = curl_init();
+
+        $options = array( 
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_SSL_VERIFYHOST => false,
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_USERAGENT => $this->userAgent,
+            CURLOPT_VERBOSE => true,
+            CURLOPT_URL => $this->url,
+            CURLOPT_SSLCERT => getcwd().$this->certificate,
+            CURLOPT_SSLCERTPASSWD => 'password',
+            CURLOPT_SSLKEY => getcwd().$this->key,
+            CURLOPT_SSLKEYPASSWD => 'password',
+        );
+ 
+        curl_setopt_array($ch , $options);
+
+        $output = curl_exec($ch);
+
+        if(!$output) {
+            $this->setError(curl_error($ch));
+        } else {
+            $this->setData($output);
+        }
+        
+        return $this->response;
+    }
+    
+    private function setData($data) {
+        $this->response->data = $data;
+    }
+    
+    private function setError($error) {
+        $this->response->error = $error;
+    }
+}
